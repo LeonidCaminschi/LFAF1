9d57c62542e252701d6ec30382637010f1b37a2f
LeonidCaminschi <leocer@leonid.gmail.com>
2023-04-19 23:02:31 +0300

    Added conversion to Chomsky normal form

--- src/grammar.h
+++ src/grammar.h
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <csignal>
 #include <set>
+#include <unordered_set>
+#include <map>
 
 using namespace std;
 
@@ -161,5 +163,277 @@ struct Grammar {
 //        cout << endl;
 
         startingCharacter = "S";
+    }
+
+    void toCNF() {
+        eliminateEpsilonProductions();
+        eliminateRenaming();
+        eliminateInaccessibleSymbols();
+        eliminateNonproductiveSymbols();
+        convertToChomskyNormalForm();
+
+        // Sort the rules vector
+        std::sort(productions.begin(), productions.end(), [](const Rule& a, const Rule& b) {
+            if (a.Left == b.Left) {
+                return a.Right < b.Right;
+            }
+            return a.Left < b.Left;
+        });
+
+        // Use std::unique to remove duplicates
+        auto it = std::unique(productions.begin(), productions.end(), [](const Rule& a, const Rule& b) {
+            return a.Left == b.Left && a.Right == b.Right;
+        });
+
+        // Erase the duplicates from the vector
+        productions.erase(it, productions.end());
+
+        // Print the rules vector to verify the duplicates are removed
+        for (const auto& r : productions) {
+            std::cout << r.Left << " -> " << r.Right << std::endl;
+        }
+    }
+
+private:
+    void eliminateEpsilonProductions() {
+        unordered_set<string> epsilonVars;
+
+        // Find variables that can derive ε
+        for (const auto& r : productions) {
+            if (r.Right == "ε") {
+                epsilonVars.insert(r.Left);
+            }
+        }
+
+        // Eliminate ε productions
+        while (!epsilonVars.empty()) {
+            string var = *epsilonVars.begin();
+            epsilonVars.erase(epsilonVars.begin());
+
+            // Remove ε productions with var on the right-hand side
+            for (auto it = productions.begin(); it != productions.end(); ) {
+                if (it->Right == "ε") {
+                    it = productions.erase(it);
+                } else if (it->Right.find(var) != string::npos) {
+                    // Generate all possible combinations of the right-hand side without var
+                    vector<string> combinations = {""};
+                    for (const auto& c : it->Right) {
+                        if (string(1, c) == var) {
+                            size_t n = combinations.size();
+                            for (size_t i = 0; i < n; i++) {
+                                combinations.push_back(combinations[i]);
+                                combinations.back().push_back(c);
+                                combinations[i].push_back(' ');
+                            }
+                        } else {
+                            for (auto& s : combinations) {
+                                s.push_back(c);
+                            }
+                        }
+                    }
+
+                    // Add the new productions
+                    for (const auto& s : combinations) {
+                        if (s != it->Right) {
+                            productions.push_back({it->Left, s});
+                        }
+                    }
+
+                    // Remove the original production
+                    it = productions.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+
+            // Remove ε productions with var on the left-hand side
+            for (auto it = productions.begin(); it != productions.end(); ) {
+                if (it->Left == var && it->Right == "ε") {
+                    it = productions.erase(it);
+                } else if (it->Right.find(var) != string::npos) {
+                    productions.push_back({it->Left, it->Right});
+                    it = productions.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+        }
+    }
+
+    void eliminateRenaming() {
+        // Find all variables that can be directly replaced by another variable
+        map<string, vector<vector<string>>> replacements;
+        for (const auto& r : productions) {
+            if (r.Right.size() == 1 && isupper(r.Right[0])) {
+                replacements.emplace(r.Left, vector<vector<string>>{{r.Right}});
+            } else {
+                vector<vector<string>> right_parts;
+                vector<string> current_part;
+                for (char c : r.Right) {
+                    if (isupper(c)) {
+                        if (!current_part.empty()) {
+                            right_parts.push_back(current_part);
+                            current_part.clear();
+                        }
+                        current_part.push_back(string(1, c));
+                    } else {
+                        current_part.push_back(string(1, c));
+                    }
+                }
+                if (!current_part.empty()) {
+                    right_parts.push_back(current_part);
+                }
+                if (replacements.count(r.Left) == 0) {
+                    replacements.emplace(r.Left, vector<vector<string>>{right_parts[0]});
+                } else {
+                    replacements[r.Left].push_back(right_parts[0]);
+                }
+                for (size_t i = 1; i < right_parts.size(); i++) {
+                    replacements[r.Left].push_back(right_parts[i]);
+                }
+            }
+        }
+
+        // Replace each nonterminal with its expansions
+        vector<Rule> new_productions;
+        for (const auto& r : productions) {
+            if (replacements.count(r.Left) == 0) {
+                new_productions.push_back(r);
+            } else {
+                vector<vector<string>> replacement_parts = replacements[r.Left];
+                for (const auto& p : replacement_parts) {
+                    string new_right;
+                    for (const auto& s : p) {
+                        new_right += s;
+                    }
+                    new_productions.push_back({r.Left, new_right});
+                }
+            }
+        }
+
+        // Update the grammar with the new productions
+        productions = new_productions;
+    }
+
+    void eliminateInaccessibleSymbols() {
+        // Start with the starting symbol
+        vector<string> reachable_symbols{startingCharacter};
+
+        // Repeat until no new reachable symbols are found
+        bool found_new_symbol = true;
+        while (found_new_symbol) {
+            found_new_symbol = false;
+            for (const auto& r : productions) {
+                // If the left-hand side of the production is reachable,
+                // add its right-hand side to the set of reachable symbols
+                if (find(reachable_symbols.begin(), reachable_symbols.end(), r.Left) != reachable_symbols.end()) {
+                    for (char c : r.Right) {
+                        if (isupper(c)) {
+                            string var(1, c);
+                            if (find(reachable_symbols.begin(), reachable_symbols.end(), var) == reachable_symbols.end()) {
+                                reachable_symbols.push_back(var);
+                                found_new_symbol = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Remove all productions that contain unreachable symbols
+        vector<Rule> new_productions;
+        for (const auto& r : productions) {
+            bool is_reachable = true;
+            for (char c : r.Right) {
+                if (isupper(c)) {
+                    string var(1, c);
+                    if (find(reachable_symbols.begin(), reachable_symbols.end(), var) == reachable_symbols.end()) {
+                        is_reachable = false;
+                        break;
+                    }
+                }
+            }
+            if (is_reachable) {
+                new_productions.push_back(r);
+            }
+        }
+
+        // Update the grammar with the new productions
+        productions = new_productions;
+    }
+
+    void eliminateNonproductiveSymbols() {
+        // Start with all non-terminal variables
+        vector<string> productive_symbols{nonTerminalVariables};
+
+        // Repeat until no new productive symbols are found
+        bool found_new_symbol = true;
+        while (found_new_symbol) {
+            found_new_symbol = false;
+            for (const auto& r : productions) {
+                // If the left-hand side of the production is productive,
+                // add its right-hand side to the set of productive symbols
+                if (find(productive_symbols.begin(), productive_symbols.end(), r.Left) != productive_symbols.end()) {
+                    bool is_productive = true;
+                    for (char c : r.Right) {
+                        if (isupper(c)) {
+                            string var(1, c);
+                            if (find(productive_symbols.begin(), productive_symbols.end(), var) == productive_symbols.end()) {
+                                is_productive = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (is_productive) {
+                        string var = string() + r.Left;
+                        if (find(productive_symbols.begin(), productive_symbols.end(), var) == productive_symbols.end()) {
+                            productive_symbols.push_back(var);
+                            found_new_symbol = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Remove all non-productive symbols
+        vector<string> new_non_terminal_variables;
+        vector<Rule> new_productions;
+        for (const auto& var : productive_symbols) {
+            if (find(nonTerminalVariables.begin(), nonTerminalVariables.end(), var) != nonTerminalVariables.end()) {
+                new_non_terminal_variables.push_back(var);
+            }
+            for (const auto& r : productions) {
+                if (r.Left == var || find(r.Right.begin(), r.Right.end(), var[0]) != r.Right.end()) {
+                    new_productions.push_back(r);
+                }
+            }
+        }
+
+        // Update the grammar with the new non-terminal variables and productions
+        nonTerminalVariables = new_non_terminal_variables;
+        productions = new_productions;
+    }
+
+    void convertToChomskyNormalForm() {
+        map<string, int> indices;
+        int index = 0;
+        for (const string& var : nonTerminalVariables) {
+            indices[var] = index++;
+        }
+
+        for (Rule& r : productions) {
+            if (r.Right.size() > 2) {
+                string newVar = "X" + to_string(index++);
+                int length = r.Right.length();
+                for (int i = 0; i < length - 2; i++) {
+                    string newRuleVar = (i == 0) ? r.Left : newVar;
+                    string newRuleRight = r.Right.substr(i, 2);
+                    productions.push_back({ newRuleVar, newRuleRight });
+                    r.Right.replace(i, 2, newRuleVar);
+                }
+                productions.push_back({ newVar, r.Right.substr(length - 2, 2) });
+                r.Right.replace(length - 2, 2, newVar);
+            }
+        }
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -48,15 +48,46 @@ int main() {
 //
 //    cout << "This automata is: " << (automata2.isDeterministic() ? "Deterministic" : "Non-Deterministic") << endl;
 //    automata2.convertNFAToDFA();
+//
+//    string expression = "2*((3+4)/5)/13.451";
+//    Lexer lexer(expression);
+//    vector<Token> tokens = lexer.tokenize();
+//    if (expression.length() > 0 && tokens.size() == 0) {
+//        cout << "An error occured please revisit the initial expression" << endl;
+//    } else {
+//        for (Token token: tokens) {
+//            cout << token << endl;
+//        }
+//    }
 
-    string expression = "2*((3+4)/5)/13.451";
-    Lexer lexer(expression);
-    vector<Token> tokens = lexer.tokenize();
-    if (expression.length() > 0 && tokens.size() == 0) {
-        cout << "An error occured please revisit the initial expression" << endl;
-    } else {
-        for (Token token: tokens) {
-            cout << token << endl;
-        }
-    }
+
+    Grammar test;
+    test.terminalVariables.push_back("S");
+    test.terminalVariables.push_back("A");
+    test.terminalVariables.push_back("B");
+    test.terminalVariables.push_back("C");
+    test.terminalVariables.push_back("E");
+
+    test.nonTerminalVariables.push_back("a");
+    test.nonTerminalVariables.push_back("b");
+
+    test.startingCharacter = "S";
+
+    test.productions.push_back(Rule("S", "bA"));
+    test.productions.push_back(Rule("S", "B"));
+    test.productions.push_back(Rule("A", "a"));
+    test.productions.push_back(Rule("A", "aS"));
+    test.productions.push_back(Rule("A", "bAaAb"));
+    test.productions.push_back(Rule("B", "AC"));
+    test.productions.push_back(Rule("B", "bS"));
+    test.productions.push_back(Rule("B", "aAa"));
+    test.productions.push_back(Rule("C", "ε"));
+    test.productions.push_back(Rule("C", "AB"));
+    test.productions.push_back(Rule("E", "BA"));
+
+    test.toCNF();
+
+//    for (auto it : test.productions) {
+//        cout << it << endl;
+//    }
 }
